8e05e90809f411faea2db11481e711ae8876245a
ch <chenhuan1@akulaku.com>
2021-11-27 10:30:08 +0800

    feat: update

--- src/views/index.vue
+++ src/views/index.vue
@@ -4,25 +4,30 @@
       <dv-loading v-if="loading">Loading...</dv-loading>
       <div v-else class="host-body">
         <div class="d-flex jc-center">
-          <dv-decoration-10 style="width:33.3%;height:.0625rem;" />
+          <dv-decoration-10 style="width: 33.3%; height: 0.0625rem" />
           <div class="d-flex jc-center">
-            <dv-decoration-8 :color="['#568aea', '#000000']" style="width:2.5rem;height:.625rem;" />
+            <dv-decoration-8
+              :color="['#568aea', '#000000']"
+              style="width: 2.5rem; height: 0.625rem"
+            />
             <div class="title">
               <span class="title-text">平吉制糖有限公司</span>
               <dv-decoration-6
                 class="title-bototm"
                 :reverse="true"
                 :color="['#50e3c2', '#67a1e5']"
-                style="width:3.125rem;height:.1rem;"
+                style="width: 3.125rem; height: 0.1rem"
               />
             </div>
             <dv-decoration-8
               :reverse="true"
               :color="['#568aea', '#000000']"
-              style="width:2.5rem;height:.625rem;"
+              style="width: 2.5rem; height: 0.625rem"
             />
           </div>
-          <dv-decoration-10 style="width:33.3%;height:.0625rem; transform: rotateY(180deg);" />
+          <dv-decoration-10
+            style="width: 33.3%; height: 0.0625rem; transform: rotateY(180deg)"
+          />
         </div>
 
         <!-- 第二行 -->
@@ -30,12 +35,16 @@
           <div class="d-flex" style="width: 40%">
             <div
               class="react-right ml-4"
-              style="width: 6.25rem; text-align: left;background-color: #0f1325;"
+              style="
+                width: 6.25rem;
+                text-align: left;
+                background-color: #0f1325;
+              "
             >
               <span class="react-before"></span>
               <span class="text">平吉制糖厂生产数据大屏</span>
             </div>
-            <div class="react-right ml-3" style="background-color: #0f1325;">
+            <div class="react-right ml-3" style="background-color: #0f1325">
               <span class="text colorBlue">数据分析</span>
             </div>
           </div>
@@ -45,10 +54,14 @@
             </div>
             <div
               class="react-left mr-4"
-              style="width: 6.25rem; background-color: #0f1325; text-align: right;"
+              style="
+                width: 6.25rem;
+                background-color: #0f1325;
+                text-align: right;
+              "
             >
               <span class="react-after"></span>
-              <span class="text">{{currtime}}</span>
+              <span class="text">{{ currtime }}</span>
             </div>
           </div>
         </div>
@@ -56,12 +69,13 @@
         <div class="body-box">
           <!-- 第三行数据 -->
           <div class="content-box">
-            <!-- 中间 -->
-            <div>
+            <dv-border-box-13>
               <center />
-            </div>
-            <!-- 中间 -->
-            <div>
+            </dv-border-box-13>
+            <!-- <div>
+              <center />
+            </div> -->
+            <!-- <div>
               <centerRight2 />
             </div>
             <div>
@@ -73,12 +87,10 @@
               <dv-border-box-12>
                 <centerLeft1 :data="numberData2" id="ring2"/>
               </dv-border-box-12>
-            </div>
-            <div>
-              <dv-border-box-13>
-                <centerRight1 />
-              </dv-border-box-13>
-            </div>
+            </div> -->
+            <dv-border-box-13>
+              <centerRight1 />
+            </dv-border-box-13>
           </div>
 
           <!-- 第四行数据 -->
@@ -97,10 +109,10 @@
 </template>
 
 <script>
-import centerLeft1 from "./centerLeft1";
-// import centerLeft2 from "./centerLeft2";
+// import centerLeft1 from "./centerLeft1";
+// // import centerLeft2 from "./centerLeft2";
 import centerRight1 from "./centerRight1";
-import centerRight2 from "./centerRight2";
+// import centerRight2 from "./centerRight2";
 import center from "./center";
 import bottomLeft from "./bottomLeft";
 import bottomRight from "./bottomRight";
@@ -115,19 +127,23 @@ export default {
           text: "温度",
         },
         {
-            value: 120,
+          value: 120,
           text: "温度",
-        }, {
-           value: 120,
+        },
+        {
+          value: 120,
           text: "温度",
-        }, {
-            value: 120,
+        },
+        {
+          value: 120,
           text: "温度",
-        }, {
-            value: 120,
+        },
+        {
+          value: 120,
           text: "温度",
-        }, {
-           value: 120,
+        },
+        {
+          value: 120,
           text: "温度",
         },
       ],
@@ -137,30 +153,35 @@ export default {
           text: "温度",
         },
         {
-            value: 120,
-          text: "温度",
-        }, {
-           value: 120,
-          text: "温度",
-        }, {
-            value: 120,
-          text: "温度",
-        }, {
-            value: 120,
-          text: "温度",
-        }, {
-           value: 120,
+          value: 120,
           text: "温度",
         },
-      ]
+        {
+          value: 120,
+          text: "温度",
+        },
+        {
+          value: 120,
+          text: "温度",
+        },
+        {
+          value: 120,
+          text: "温度",
+        },
+        {
+          value: 120,
+          text: "温度",
+        },
+      ],
     };
   },
   components: {
-    centerLeft1,
-    // centerLeft2,
+    // centerLeft1,
+    // // centerLeft2,
     centerRight1,
-    centerRight2,
+    // centerRight2,
     center,
+
     bottomLeft,
     bottomRight,
   },
@@ -184,7 +205,8 @@ export default {
         new Date().getSeconds() < 10
           ? "0" + new Date().getSeconds()
           : new Date().getSeconds();
-      _this.currtime = yy + "-" + mm + "-" + dd + " " + hh + ":" + mf + ":" + ss;
+      _this.currtime =
+        yy + "-" + mm + "-" + dd + " " + hh + ":" + mf + ":" + ss;
     },
     currentTime() {
       setInterval(this.getTime, 500);
